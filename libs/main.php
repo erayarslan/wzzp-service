a0391b21dfac8acccd82f63a9fce087b19f766fc
Eray ARSLAN <relfishere@gmail.com>
2014-06-23 17:42:58 +0300

    updated.

--- libs/main.php
+++ libs/main.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/../utils/constants.php';
 require_once dirname(__FILE__) . '/../configs/db.php';
-require_once dirname(__FILE__) . '/../third-party/rb/rb.php';
+require dirname(__FILE__) . '/../third-party/rb/rb.phar';
 
 class main {
     public function __construct() {
