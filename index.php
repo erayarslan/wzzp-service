13210921a8fd9f85c47c35de2d7b26c57661c16b
Eray ARSLAN <relfishere@gmail.com>
2014-06-23 18:49:37 +0300

    updated.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
 
 require dirname(__FILE__) . '/third-party/Slim/Slim.php';
-require dirname(__FILE__) . '/utils/optionsFucker';
+require dirname(__FILE__) . '/utils/optionsFucker.php';
 require dirname(__FILE__) . '/libs/main.php';
 
 \Slim\Slim::registerAutoloader();
