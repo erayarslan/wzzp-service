30060ab3ee119e3a259f5a323516c4782a056aff
Eray ARSLAN <relfishere@gmail.com>
2014-06-24 19:28:38 +0300

    updated.

--- libs/user.php
+++ libs/user.php
@@ -2,6 +2,10 @@
 require dirname(__FILE__) . '/../third-party/rb/rb-p533.php';
 
 class user {
+    public function __construct() {
+        R::setup('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+    }
+
     public function getUserByUsername($username) {
         $user = R::findOne(
             constants::db_users,
