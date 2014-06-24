ef79eea42fb7c7540c534a96ea2d27bd5da6a99c
Eray ARSLAN <relfishere@gmail.com>
2014-06-24 19:18:09 +0300

    updated.

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, '.constants::bad_a
 
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    "debug" => true
+));
 $app->main = new main();
 
 $app->hook(constants::slimBeforeRouter, function () use ($app) {
