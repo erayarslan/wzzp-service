32e397698fd47e578e6a6239a18e8cbe12299512
Eray ARSLAN <relfishere@gmail.com>
2014-06-24 10:14:38 +0300

    updated.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require dirname(__FILE__) . '/third-party/Slim/Slim.php';
 require dirname(__FILE__) . '/libs/main.php';
-require dirname(__FILE__) . '/utils/constants.php';
+require_once dirname(__FILE__) . '/utils/constants.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, wzzp_token');
